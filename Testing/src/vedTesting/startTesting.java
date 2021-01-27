6a5a2ca722ebc6ec88a5aacce98a3e396ba97bb4
User <User@192.168.2.22>
2021-01-27 07:57:27 -0500

    checking commit

--- Testing/src/vedTesting/startTesting.java
+++ Testing/src/vedTesting/startTesting.java
@@ -5,6 +5,10 @@ public class startTesting {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
+		int i =10;
+		int z =10;
+		int x = i+z;
+		System.out.println(x);
 	}
 
 }
